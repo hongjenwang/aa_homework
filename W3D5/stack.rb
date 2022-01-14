aa9f494e23b4c7d4544f98cf52f17b0f7cec96d8
Victor Wang <hongjenwang@gmail.com>
2022-01-14 07:52:58 -0800

    Add Queue class

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -1,18 +1,41 @@
 class Stack
     def initialize
-      @the_array = []
+        @the_array = []
     end
 
     def push(el)
-      @the_array.push(el)
-      el
+        @the_array.push(el)
+        el
     end
 
     def pop
-      @the_array.pop
+        @the_array.pop
     end
 
     def peek
-      @the_array[-1]
+        @the_array[-1]
     end
   end
+
+
+class Queue
+    def initialize
+        @queue_arr = []
+    end
+
+    def enqueue(el)
+        @queue_arr.push(el)
+        el
+    end
+
+    def dequeue
+        @queue_arr.shift
+    end
+
+    def peek
+        @queue_arr.first
+    end
+
+end
+
+
