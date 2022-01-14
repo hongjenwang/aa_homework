af70a004d057cd92da2972fc0d4646acca806ee1
Victor Wang <hongjenwang@gmail.com>
2022-01-14 08:35:26 -0800

    Add class Map

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -39,3 +39,46 @@ class Queue
 end
 
 
+class Map
+
+    def initialize
+        @underlying_array = []
+    end
+
+    def set(key, value) 
+        #get index of the kv if key is found in the underlying arry
+        pair_idx = @underlying_array.index { |pair| pair[0] == key}
+        #if it exists, update the value of the index to the value passed in
+        #if not push it in the array
+        if pair_idx
+            @underlying_array[pair_idx][1] == value
+        else
+            @underlying_array.push([key, value])
+        end
+        value
+    end
+
+    # a key is passed in to get its value. 
+    def get(key) 
+        @underlying_array.each { |pair| return pair[1] if pair[0] == key  }
+        nil
+    end
+
+
+    def delete(key)
+        @underlying_array.reject{ |pair| pair[0] == key }
+    end
+
+    def show
+
+    end
+
+
+
+
+
+
+
+
+
+end
